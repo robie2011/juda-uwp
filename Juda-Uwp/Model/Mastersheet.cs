4d45befc534d9b27c509383b1e48a031f7fb7b84
Robert Rajakone <robie2011@gmail.com>
2016-03-16 16:52:06 +0100

    Adding Domain Models

---
+++ Juda-Uwp/Model/Mastersheet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juda_Uwp.Model
+{
+    public enum SongPartType
+    {
+        Vers1,
+        Vers2,
+        Vers3,
+        Vers4,
+        Vers5,
+        Vers6,
+        Chorus,
+        PreChorus,
+        Bridge,
+        Intro
+    }
+
+    public class SongPart
+    {
+        public SongPartType SongPartType { get; set; }
+        public string[] Lines { get; set; }
+    }
+
+    public class LanguageVersion
+    {
+        public Languages Language { get; set; }
+        public SongPart[] SongParts { get; set; }
+    }
+
+    public class Mastersheet
+    {
+        public int SongId { get; set; }
+        public LanguageVersion[] LanguageVersions { get; set; }
+    }
+}
---
+++ Juda-Uwp/Model/SongModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juda_Uwp.Model
+{
+    public class Song
+    {
+        //todo: removing setter?
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Artist Artist { get; set; }
+        public Languages Language { get; set; }
+    }
+}
